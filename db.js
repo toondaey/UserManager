1b10167b6b43393b1467efafb04cb209af1682f5
Tunde <tunde@cchubnigeria.com>
2017-12-15 16:06:14 +0100

    Update to environment variables.

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    db = `mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+    db = `mongodb://${process.env.DB_URL}/${process.env.DB_NAME}`;
 
 mongoose.connect(
     db,
